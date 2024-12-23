38bcb168968783f285fef8220295083143639606
unknown <jaykergg@gmail.com>
2024-12-23 09:03:56 +0700

    tesst assignment2 ThanhLONG

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TodoComponent } from "./todo/todo.component";
+import{Assignment2Component} from './assignment2/assignment2.component'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TodoComponent],
+  imports: [RouterOutlet, TodoComponent,Assignment2Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
