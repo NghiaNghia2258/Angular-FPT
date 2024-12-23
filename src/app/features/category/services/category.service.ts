84d604efb972f252950fc19eb12ce81da27963c6
nghianghia <ngonghia2258@gmail.com>
2024-12-23 14:27:21 +0700

    *

--- src/app/features/category/services/category.service.ts
+++ src/app/features/category/services/category.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { RequestCategory } from '../models/add-category-requset.model';
 import { Observable } from 'rxjs';
+import { RequestCategoryUpdate } from '../models/update-category-requset.model';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,11 @@ export class CategoryService {
 
   addCategory(model:RequestCategory):Observable<void>{
     return this.http.post<void>('https://localhost:7011/api/CategoryCTRL/Create',model);
+  }
+  updateCategory(model:RequestCategoryUpdate):Observable<void>{
+    return this.http.put<void>(`https://localhost:7011/api/CategoryCTRL/update_category?id=${model.id}`,model);
+  }
+  getbyidCategory(id:string):Observable<any>{
+    return this.http.get<any>(`https://localhost:7011/api/Category/Get_BY_ID?id=${id}`);
   }
 }
