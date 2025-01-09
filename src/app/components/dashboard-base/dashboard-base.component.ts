9f4b7bd4a7b1977390d76fa02f2af5693c750da0
NghiaNghia <ngonghia2258@gmail.com>
2025-01-09 20:58:24 +0700

    *

--- src/app/components/dashboard-base/dashboard-base.component.ts
+++ src/app/components/dashboard-base/dashboard-base.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../../core/navbar/navbar.component";
 
 @Component({
   selector: 'app-dashboard-base',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './dashboard-base.component.html',
   styleUrl: './dashboard-base.component.css'
 })
