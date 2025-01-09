664e6bc1b2fc5ada26b4392b558fdd300d171a27
NghiaNghia <ngonghia2258@gmail.com>
2025-01-09 20:57:51 +0700

    *

---
+++ src/app/components/dashboard-base/dashboard-base.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-base',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-base.component.html',
+  styleUrl: './dashboard-base.component.css'
+})
+export class DashboardBaseComponent {
+
+}
--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,27 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { MenuModule } from 'primeng/menu';
+import { BadgeModule } from 'primeng/badge';
+import { RippleModule } from 'primeng/ripple';
+import { AvatarModule } from 'primeng/avatar';
+import { CommonModule } from '@angular/common';
 
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { AutoCompleteModule } from 'primeng/autocomplete';
-
-interface AutoCompleteCompleteEvent {
-  originalEvent: Event;
-  query: string;
-}
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [AutoCompleteModule, FormsModule],
+  imports: [MenuModule, BadgeModule, RippleModule, AvatarModule,CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent  {
-  items: any[] | undefined;
+export class NavbarComponent implements OnInit {
+  items: MenuItem[] | undefined;
 
-    selectedItem: any;
-
-    suggestions: any[] | undefined;
-
-    search(event: AutoCompleteCompleteEvent) {
-        this.suggestions = [...Array(10).keys()].map(item => event.query + '-' + item);
+    ngOnInit() {
+        this.items = [
+            {
+                separator: true
+            },
+            {
+                label: 'Documents',
+                items: [
+                    {
+                        label: 'New',
+                        icon: 'pi pi-plus',
+                        shortcut: '⌘+N'
+                    },
+                    {
+                        label: 'Search',
+                        icon: 'pi pi-search',
+                        shortcut: '⌘+S'
+                    }
+                ]
+            },
+            {
+                label: 'Profile',
+                items: [
+                    {
+                        label: 'Settings',
+                        icon: 'pi pi-cog',
+                        shortcut: '⌘+O'
+                    },
+                    {
+                        label: 'Messages',
+                        icon: 'pi pi-inbox',
+                        badge: '2'
+                    },
+                    {
+                        label: 'Logout',
+                        icon: 'pi pi-sign-out',
+                        shortcut: '⌘+Q'
+                    }
+                ]
+            },
+            {
+                separator: true
+            }
+        ];
     }
 }
